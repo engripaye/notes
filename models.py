309f80a7dec510b1dc208b28c48f77338a1a6532
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-13 13:13:17 +0100

    backend code commit by ENGR. IPAYE

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=True)
+    filename = Column(String, nullable=True)
+    user_id = Column(String, nullable=False)
